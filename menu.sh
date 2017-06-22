bc614912dc918eb3a799741ba96aebab2820cbf3
yonatankanu <nugraha_yonatan@yahoo.co.id>
2017-06-22 15:28:40 +0700

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Menu
+
+echo -e "============MENU=============="
+echo -e "* menu       : menampilkan daftar perintah yang tersedia"
+echo -e "* usernew    : membuat akun SSH"
+echo -e "* trial      : membuat akun trial"
+echo -e "* hapus      : menghapus akun SSH"
+echo -e "* login      : cek user login"
+echo -e "* member     : cek member SSH"
+echo -e "* resvis     : restart service dropbear, webmin, squid3, openvpn dan ssh"
+echo -e "* reboot     : reboot VPS"
+echo -e "* speedtest  : speedtest VPS"
+echo -e "* about      : informasi tentang script auto install"
+echo -e "=========THORN SSH============="
