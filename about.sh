cd2b96274a35ecf5152471f5fe544abf7ff92424
yonatankanu <nugraha_yonatan@yahoo.co.id>
2017-06-22 15:36:54 +0700

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#About this script
+
+IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+echo -e ""
+echo -e "===================================================="
+echo -e "Script Auto Install SSH" 
+echo -e "===================================================="
+echo -e ""
+echo -e "For Debian 7 32 bit"
+echo -e "VPS DEBIAN JOSS"
+echo -e ""
+echo -e "Original script by :"
+echo -e "* Siapa yak ?"
+echo -e "* Yang Penting Lancar yah :v"
+echo -e "* Yonatan Kanu as THORN SSH"
+echo -e ""
+echo -e "Modified by Yonatan Kanu as THORN SSH"
+echo -e ""
+echo -e "Contact Person : https://www.facebook.com/kanu.nugraha"
+echo -e "Website        : http://yonatankanu.blogspot.co.id atau http://yonatankanu.wordpress.com/"
+echo -e ""
+
