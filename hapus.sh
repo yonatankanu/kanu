b02a202cc5fbcecd29f9f513ea992cc22fb4206d
yonatankanu <nugraha_yonatan@yahoo.co.id>
2017-06-22 19:36:01 +0700

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -5,3 +5,9 @@
 read -p "Nama user SSH yang akan dihapus : " Nama
 
 userdel -r $Nama
+
+echo -e "ALL SUPPORTED by Yonatan Kanu as THORN SSH"
+echo -e "WA : 085707136028"
+echo -e "FB: https://www.facebook.com/kanu.nugraha"
+echo -e "BLOG : http://yonatankanu.blogspot.co.id/"
+echo -e "BLOG : http://yonatankanu.wordpress.com/"
