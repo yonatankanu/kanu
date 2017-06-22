b6cf83e416b0510ffc46ae08915877a14e911513
yonatankanu <nugraha_yonatan@yahoo.co.id>
2017-06-22 19:34:55 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -3,6 +3,7 @@
 
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 echo -e ""
+echo -e "PENASARAN NIH YA.....SEMOGA PUAS DENGAN AUTO SAYA"
 echo -e "===================================================="
 echo -e "Script Auto Install SSH" 
 echo -e "===================================================="
@@ -18,6 +19,6 @@ echo -e ""
 echo -e "Modified by Yonatan Kanu as THORN SSH"
 echo -e ""
 echo -e "Contact Person : https://www.facebook.com/kanu.nugraha"
+echo -e "WhatsApp       : 085707136028
 echo -e "Website        : http://yonatankanu.blogspot.co.id atau http://yonatankanu.wordpress.com/"
-echo -e ""
-
+echo -e "SARAN DAN KRITIK ? PERSONAL MESSAGE MANG BIAR MESRA :V"
