1daa4aeb4b8c8cd2cd05e9b3f1bb4d5c8712c0d9
yonatankanu <nugraha_yonatan@yahoo.co.id>
2017-06-25 14:26:42 +0700

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -27,4 +27,5 @@ echo -e "Contact Person : https://www.facebook.com/kanu.nugraha"
 echo -e "WhatsApp       : 085707136028"
 echo -e "Website        : http://yonatankanu.blogspot.co.id"
 echo -e "Website        : http://yonatankanu.wordpress.com/"
+
 echo -e ""
