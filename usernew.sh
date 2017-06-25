ecc23695393bdcaf4eae1f07b9ee5227b60edeb8
yonatankanu <nugraha_yonatan@yahoo.co.id>
2017-06-25 14:25:48 +0700

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -6,9 +6,9 @@ read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
 
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
-useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
-exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
-echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M thornssh-$Login
+exp="$(chage -l thornssh-$Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd thornssh-$Login &> /dev/null
 echo -e ""
 echo -e "====Informasi SSH account===="
 echo -e "Host: $IP" 
@@ -16,7 +16,7 @@ echo -e "Port OpenSSH: 22,143"
 echo -e "Port Dropbear: 80,109,110443"
 echo -e "Squid: 8080,3128"
 echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
-echo -e "Username: $Login "
+echo -e "Username: thornssh-$Login "
 echo -e "Password: $Pass"
 echo -e "-----------------------------"
 echo -e "Aktif Sampai: $exp"
@@ -27,4 +27,5 @@ echo -e "Contact Person : https://www.facebook.com/kanu.nugraha"
 echo -e "WhatsApp       : 085707136028"
 echo -e "Website        : http://yonatankanu.blogspot.co.id"
 echo -e "Website        : http://yonatankanu.wordpress.com/"
+
 echo -e ""
