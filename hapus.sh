04ea926572275fab8a118341ea6dc0f6e5272e28
yonatankanu <nugraha_yonatan@yahoo.co.id>
2017-06-25 17:41:30 +0700

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-# Hapus user SSH kuy.....
-# Created by Yonatan Kanu as THORN SSH
+echo "-------------------------------"
+echo "USERNAME          EXP DATE     "
+echo "-------------------------------"
+while read expired
+do
+        AKUN="$(echo $expired | cut -d: -f1)"
+        ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+        exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+        if [[ $ID -ge 1000 ]]; then
+        printf "%-17s %2s\n" "$AKUN" "$exp"
+        fi
+done < /etc/passwd
+JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+echo "-------------------------------"
+echo "Jumlah akun: $JUMLAH user"
+echo "-------------------------------"
 
+echo -e ""
 read -p "Nama user SSH yang akan dihapus : " Nama
 
 userdel -r $Nama
